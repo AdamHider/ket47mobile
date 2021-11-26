776cf3ce773dcd4b5de7ea3c8e347dee80930e8e
AdamHider <ajder.adjivapov@gmail.com>
2021-11-26 17:00:28 +0300

    Create

---
+++ src/components/HomeSlider.vue
@@ -0,0 +1,50 @@
+<template>
+    <ion-slides pager="true" :options="slideOpts">
+        <ion-slide v-for="slide in slideList" :key="slide.id">
+            <div class="slide">
+                <img  :src="slide.image"/>
+            </div>
+        </ion-slide>
+    </ion-slides>
+</template>
+
+<script>
+import { IonSlides, IonSlide } from '@ionic/vue';
+
+export default{
+    components: { IonSlides, IonSlide },
+    setup() {
+        const slideOpts = {  
+            slidesPerView: 1,
+            centeredSlides: true, 
+            initialSlide: 0,
+            speed: 400,
+            grabCursor: true,
+            touchStartForcePreventDefault: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        };
+        return { slideOpts }
+    },
+    data(){
+        return {
+            slideList: [
+                {
+                    id: 1,
+                    image: "http://psdcollection.ru/wp-content/uploads/2020/01/banner-vesna-1.png"
+                },
+                {
+                    id: 2,
+                    image: "http://psdcollection.ru/wp-content/uploads/2020/01/banner-vesna-2.png"
+                },
+                {
+                    id: 3,
+                    image: "http://psdcollection.ru/wp-content/uploads/2020/01/banner-vesna-3.png"
+                }
+            ]
+        }
+    }
+};
+</script>
---
+++ src/views/UserAddresses.vue
@@ -0,0 +1,97 @@
+<template>
+  <base-layout page-title="Мои адреса" page-default-back-link="/user-dashboard" :errorMessage="error">
+  <div class="panel">
+  <ion-list class="adresses-list">
+    <ion-item  v-for="adress in addressList" :key="adress.id">
+        <ion-thumbnail>
+          <ion-icon v-if="adress.type == 'home'" name="home"></ion-icon>
+          <ion-icon v-if="adress.type == 'work'" name="business"></ion-icon>
+          <ion-icon v-if="adress.type == 'other'" name="trail-sign"></ion-icon>
+        </ion-thumbnail>
+        <ion-card-header>
+            <ion-label>{{ adress.address }}, {{ adress.city }}</ion-label>
+        </ion-card-header>
+        <ion-thumbnail>
+          <ion-icon name="trash"></ion-icon>
+        </ion-thumbnail>
+    </ion-item>
+  </ion-list>
+  </div>
+  </base-layout>
+</template>
+
+
+<script>
+
+import { 
+  IonIcon,
+  IonThumbnail
+} from '@ionic/vue';
+import { addIcons } from 'ionicons';
+import { home, business, trailSign, trash } from 'ionicons/icons';
+
+
+export default  {
+  name: 'UserAddresses',
+  components: {
+    IonIcon,
+    IonThumbnail
+  },
+  created() {
+    addIcons({
+      'home': home,
+      'business': business,
+      'trail-sign': trailSign,
+      'trash': trash
+    });
+  },
+  data(){
+    return{
+      addressList: [
+        {
+          id: 1,
+          type: "home",
+          city: "Las Vegas",
+          address: "Casino Royale 2",
+          coordinates: {
+            longitude: "-77.0364",
+            latitude: "38.8951"
+          }
+        },
+        {
+          id: 1,
+          type: "work",
+          city: "Washington DC",
+          address: "White House 1",
+          coordinates: {
+            longitude: "-77.0364",
+            latitude: "38.8951"
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+.panel{
+  box-shadow: 1px 1px 15px -5px #00000085;
+}
+ ion-list.adresses-list{
+  padding-bottom: 0px;
+}
+.adresses-list ion-card{
+  display: flex;
+  justify-content: space-between;
+}
+.adresses-list ion-card-header{
+  flex: 3;
+}
+.adresses-list ion-icon {
+  font-size: 26px;
+  padding: 12px;
+}
+</style>
+
