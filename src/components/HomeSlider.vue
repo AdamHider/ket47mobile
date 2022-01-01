d86f4031627651d721e0fc4b76a9033cad802f77
baycik <baycikplus@gmail.com>
2022-01-01 12:23:42 +0300

    working on slider

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -1,5 +1,5 @@
 <template>
-      <swiper :modules="modules" :autoplay='{delay: 300,disableOnInteraction: false}' :loop="true"  effect="fade">
+      <swiper :modules="modules" :autoplay='{delay: 300, disableOnInteraction: false}' :loop="true" effect="fade">
         <swiper-slide v-for="hslide in home_slides" :key="hslide.title" :style="`background-color:${hslide.color}`">
           <img  :src="hslide.image" style="margin-left:20%;"/>
           <div style="position:absolute;color:white;margin-left:40%;text-align:left;text-shadow: 0px 0px 3px #000000;">
@@ -11,15 +11,19 @@
 </template>
 
 <script>
-  import 'swiper/vue';
-  import '@ionic/vue/css/ionic-swiper.css';
   import jQuery from "jquery";
   import { defineComponent } from 'vue';
+
+
   import { Swiper, SwiperSlide } from 'swiper/vue';
   import { Autoplay, EffectFade  } from 'swiper';
   import SwiperCore from 'swiper';
+  import 'swiper/swiper.min.css';
+  import 'swiper/components/effect-fade';
+  import '@ionic/vue/css/ionic-swiper.css'; 
 
-  SwiperCore.use([Autoplay,EffectFade]);
+  SwiperCore.use([Autoplay, EffectFade]);
+
   export default defineComponent({
     components: {
       Swiper,
@@ -40,6 +44,10 @@
             jQuery.get( "/assets/homeslider/conf.json")
             .done(function(response) {
               self.home_slides=response.slides;
+
+
+
+              //SwiperCore.use([Autoplay,EffectFade]);
             })
             .fail(function(err) {
                 self.error = err.responseJSON.messages.error;
@@ -47,10 +55,11 @@
         }
     },
     setup() {
-      SwiperCore.use([Autoplay,EffectFade]);
+      
       return {
-        modules: [Autoplay, EffectFade], 
+        modules: [Autoplay,EffectFade], 
       };
+      
     },
   });
 </script>
